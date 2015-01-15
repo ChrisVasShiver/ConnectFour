806741aeda39a44368fa2f0355ef9a2c7ec6f063
Dylan Ye <dylan_ye@hotmail.com>
2015-01-15 13:42:31 +0100

    15-01-2015 13:42
    Added horizontal and vertical rules

--- vieropeenrij/src/main/Rules.java
+++ vieropeenrij/src/main/Rules.java
@@ -1,46 +1,73 @@
 package main;
+
 /**
  * 
  * @author C. Visscher and D. Ye
  *
  */
 public class Rules {
-	
+
 	private Board currentBoard;
-	
+	public static final int WINNERSBLOCK = 4;
+
 	public Rules(Board board) {
 		currentBoard = board;
 	}
-	
+
 	public boolean isBoardFull() {
-		for(int i = 0; i < Board.MAXFIELDS; i++) {
-			if(currentBoard.getField(i) == Mark.EMPTY) {
+		for (int i = 0; i < Board.MAXFIELDS; i++) {
+			if (currentBoard.getField(i) == Mark.EMPTY) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public boolean horizontalWin(Mark m) {
-		// TODO
+		for (int col = 0; col < currentBoard.HEIGHT; col++) {
+			for (int row = 0; row < WINNERSBLOCK; row++) {
+				int countMark = 0;
+				for (int i = 0; i < WINNERSBLOCK; i++) {
+					if (currentBoard.getField(
+							currentBoard.matrixToIndex(col + i, row)).equals(m)) {
+						countMark++;
+					}
+					if (countMark == WINNERSBLOCK) {
+						return true;
+					}
+				}
+			}
+		}
 		return false;
 	}
-	
+
 	public boolean verticalWin(Mark m) {
-		// TODO
+		for (int row = 0; row < currentBoard.WIDTH; row++) {
+			for (int col = 0; col < WINNERSBLOCK; col++) {
+				int countMark = 0;
+				for (int i = 0; i < WINNERSBLOCK; i++) {
+					if (currentBoard.getField(
+							currentBoard.matrixToIndex(col, i * 7)).equals(m)) {
+						countMark++;
+					}
+					if (countMark == WINNERSBLOCK) {
+						return true;
+					}
+				}
+			}
+		}
 		return false;
 	}
-	
+
 	public boolean diagonalWin(Mark m) {
 		// TODO
 		return false;
 	}
-	
+
 	public boolean hasWinner() {
-		// TODO
-		return false;
+		return horizontalWin() || verticalWin() || diagonalWin();
 	}
-	
+
 	public boolean isGameover() {
 		return isBoardFull() || hasWinner();
 	}
