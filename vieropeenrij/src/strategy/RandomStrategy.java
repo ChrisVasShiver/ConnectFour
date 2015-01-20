610467bedb1a7ad6511044ca8bbf0005873c3f1f
dylanye <dylan_ye@hotmail.com>
2015-01-20 17:39:13 +0100

    20-01-15 17:39
    Added RandomStrategy

--- vieropeenrij/src/strategy/RandomStrategy.java
+++ vieropeenrij/src/strategy/RandomStrategy.java
@@ -1,5 +1,8 @@
 package strategy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import main.Board;
 import main.Mark;
 
@@ -9,7 +12,7 @@ import main.Mark;
  * 
  */
 public class RandomStrategy implements Strategy{
-
+	
 	@Override
 	public String getName() {
 		return "Easy";
@@ -17,7 +20,14 @@ public class RandomStrategy implements Strategy{
 
 	@Override
 	public int determineMove(Board board, Mark m) {
-		// TODO Auto-generated method stub
-		return 0;
+		Set<Integer> validmoves = new HashSet<Integer>();
+		for (int i = 0; i < 9; i++) {
+			if (board.getField(i).equals(Mark.EMPTY)) {
+				validmoves.add(i);
+			}
+		}
+		Object[] array = new Object[validmoves.size()];
+		array = validmoves.toArray();
+		return (int) array[(int) Math.floor(Math.random()*array.length)];
 	}
 }
